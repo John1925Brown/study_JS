ff62df8aa5988cead84d22434013e728829b366d
John1925Brown <dima.yaroshevskiy25@mail.ru>
2022-09-30 16:44:24 +0300

    Урок 7: Знакомимся с объектами и массивами, методы переборов и псевдомассивы
    Практика до пункта 7

--- lesson_7/index.js
+++ lesson_7/index.js
@@ -1,4 +1,3 @@
-// Было до урока 7
 let money;
 let isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
@@ -22,62 +21,105 @@ let appData = {
   deposit: false,
   mission: 5000,
   period: 5,
+  budget: money,
+  budgetDay: 0,
+  budgetMonth: 0,
+  expensesMonth: 0,
+  getExpensesMonth: function () {
+    let sum = 0;
+    let check;
+    for (let i = 0; i < 4; i++) {
+      do {
+        check = prompt('Во сколько обойдутся расходы?');
+      }
+      while (!isNumber(check));
+      sum += +check;
+    }
+    return sum;
+  },
+  getAccumulatedMonth: function () {
+    return money - expencesAmount;
+  },
+  getTargetMonth: function () {
+    if (appData.mission / accumulatedMonth < 0) {
+      return 'Цель не будет достигнута';
+    } else {
+      return appData.mission / accumulatedMonth;
+    }
+  },
+  getStatusIncome: function () {
+    if (budgetDay >= 1200) {
+      return ('У вас высокий уровень дохода');
+    } else if (budgetDay >= 600) {
+      return ('У вас средний уровень дохода');
+    } else if (budgetDay >= 0) {
+      return ('К сожалению у вас уровень дохода ниже среднего');
+    } else {
+      return ('Что то пошло не так');
+    }
+  },
   asking: function () {
     let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
     appData.addExpenses = addExpenses.toLowerCase().split(',');
     appData.deposit = confirm('Есть ли у вас депозит в банке?');
+    // appData.expences =
   }
 };
 
+appData.asking();
+
+console.log(appData);
 
 
 
-const getExpensesMonth = function () {
-  let sum = 0;
-  let check;
-  for (let i = 0; i < 4; i++) {
-    do {
-      check = prompt('Во сколько обойдутся расходы?');
-    }
-    while (!isNumber(check));
-    sum += +check;
-  }
-  return sum;
-};
+// const getExpensesMonth = function () {
+//   let sum = 0;
+//   let check;
+//   for (let i = 0; i < 4; i++) {
+//     do {
+//       check = prompt('Во сколько обойдутся расходы?');
+//     }
+//     while (!isNumber(check));
+//     sum += +check;
+//   }
+//   return sum;
+// };
 
-let expencesAmount = getExpensesMonth();
+let expencesAmount = appData.getExpensesMonth();
 
 console.log('Расходы за месяц:' + expencesAmount);
 
-const getAccumulatedMonth = function () {
-  return money - expencesAmount;
-};
+// const getAccumulatedMonth = function () {
+//   return money - expencesAmount;
+// };
 
-let accumulatedMonth = getAccumulatedMonth();
+let accumulatedMonth = appData.getAccumulatedMonth();
 
-const getTargetMonth = function () {
-  if (appData.mission / accumulatedMonth < 0) {
-    return 'Цель не будет достигнута';
-  } else {
-    return appData.mission / accumulatedMonth;
-  }
-};
+console.log(accumulatedMonth);
 
-console.log(getTargetMonth());
+// const getTargetMonth = function () {
+//   if (appData.mission / accumulatedMonth < 0) {
+//     return 'Цель не будет достигнута';
+//   } else {
+//     return appData.mission / accumulatedMonth;
+//   }
+// };
+
+console.log(appData.getTargetMonth());
 
 let budgetDay = accumulatedMonth / 30;
 
-let getStatusIncome = function () {
-  if (budgetDay >= 1200) {
-    return ('У вас высокий уровень дохода');
-  } else if (budgetDay >= 600) {
-    return ('У вас средний уровень дохода');
-  } else if (budgetDay >= 0) {
-    return ('К сожалению у вас уровень дохода ниже среднего');
-  } else {
-    return ('Что то пошло не так');
-  }
-};
-console.log(getStatusIncome());
+// let getStatusIncome = function () {
+//   if (budgetDay >= 1200) {
+//     return ('У вас высокий уровень дохода');
+//   } else if (budgetDay >= 600) {
+//     return ('У вас средний уровень дохода');
+//   } else if (budgetDay >= 0) {
+//     return ('К сожалению у вас уровень дохода ниже среднего');
+//   } else {
+//     return ('Что то пошло не так');
+//   }
+// };
 
-// Было до урока 7
+console.log(appData.getStatusIncome());
+
