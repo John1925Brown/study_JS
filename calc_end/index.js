f628110ad6723ba7d18217f7ca3b1b95cfeed750
John1925Brown <dima.yaroshevskiy25@mail.ru>
2022-12-29 22:56:43 +0300

    Урок 9: Доступ к элементам на странице. DOM + DevTools
    Практика. Доработка задания + усложненное задание

--- calc_end/index.js
+++ calc_end/index.js
@@ -4,6 +4,57 @@ let isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
+let timeA = setInterval(function () {
+  let date = new Date();
+  let months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+  let dayWeeks = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+  let hoursName = 'часов';
+  let minuteName = 'минут';
+  let secondName = 'секунд';
+  if (date.getHours() > 1 && date.getHours() < 5 || date.getHours() > 21 && date.getHours() < 25) {
+    hoursName = 'часа';
+  } else if (date.getHours() === 1 || date.getHours() === 21) {
+    hoursName = 'час';
+  }
+  if (date.getMinutes() === 1 || date.getMinutes() === 21 || date.getMinutes() === 31 || date.getMinutes() === 41 || date.getMinutes() === 51) {
+    minuteName = 'минута';
+  } else if (date.getMinutes() > 1 && date.getMinutes() < 5 || date.getMinutes() > 21 && date.getMinutes() < 25 || date.getMinutes() > 31 && date.getMinutes() < 35 || date.getMinutes() > 41 && date.getMinutes() < 45 || date.getMinutes() > 51 && date.getMinutes() < 55) {
+    minuteName = 'минуты';
+  }
+  if (date.getSeconds() === 1 || date.getSeconds() === 21 || date.getSeconds() === 31 || date.getSeconds() === 41 || date.getSeconds() === 51) {
+    secondName = 'секунда';
+  } else if (date.getSeconds() > 1 && date.getSeconds() < 5 || date.getSeconds() > 21 && date.getSeconds() < 25 || date.getSeconds() > 31 && date.getSeconds() < 35 || date.getSeconds() > 41 && date.getSeconds() < 45 || date.getSeconds() > 51 && date.getSeconds() < 55) {
+    secondName = 'секунды';
+  }
+  document.querySelector('.time-a').innerHTML = ('Сегодня ' + dayWeeks[date.getDay()] + ', ' + date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getFullYear() + ' года, ' + date.getHours() + ' ' + hoursName + ' ' + date.getMinutes() + ' ' + minuteName + ' ' + date.getSeconds() + ' ' + secondName)
+}, 1000);
+
+let timeB = setInterval(function () {
+  let date = new Date();
+  let seconds = date.getSeconds();
+  if (seconds < 10) {
+    seconds = '0' + seconds;
+  }
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = '0' + hours;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  let days = date.getDate();
+  if (days < 10) {
+    days = '0' + days;
+  }
+  let months = date.getMonth();
+  if (months < 10) {
+    months = '0' + months;
+  }
+  document.querySelector('.time-b').innerHTML = (days + '.' + (months + 1) + '.' + date.getFullYear() + ' - ' + hours + ':' + minutes + ':' + seconds);
+}, 1000);
+
+
 let start = function () {
   do {
     money = parseFloat(prompt('Ваш ежемесячный доход?'));
@@ -17,17 +68,15 @@ let buttonPlucIncome = document.querySelectorAll('button')[0];
 let buttonPlucExpences = document.querySelectorAll('button')[1];
 let checkboxDeposit = document.querySelector('#deposit-check');
 
-let incomeItemOne = document.querySelectorAll('.additional_income-item')[0]; // Логично ли переменные назвать incomeItem1/2/3... Так как может же быть не 2 расхода, а 15. Вроде было бы проще прописать, чтобы увеличивалось число в названии при создании такой же новой переменной?
-let incomeItemTwo = document.querySelectorAll('.additional_income-item')[1];
+let incomeItemOne = document.querySelectorAll('.additional_income-item')[0];
 
-let resultBudgetMonth = document.getElementsByClassName('.result-budget_month')[0];
-let resultBudgetDay = document.getElementsByClassName('.result-budget_day')[1];
-let resultExpencesMonth = document.getElementsByClassName('.result-expenses_month')[2];
-let resultAddIncome = document.getElementsByClassName('.result-additional_income')[3];
-let resultAddExpences = document.getElementsByClassName('.result-additional_expenses')[4];
-let resultIncomePeriod = document.getElementsByClassName('.result-income_period')[5];
-let resultTargetMonth = document.getElementsByClassName('.result-target_month')[6];
-
+let budgetMonthValue = document.getElementsByClassName('budget_month-value')[0];
+let budgetDayValue = document.getElementsByClassName('budget_day-value')[0];
+let expencesMonthValue = document.getElementsByClassName('expenses_month-value')[0];
+let addIncomeValue = document.getElementsByClassName('additional_income-value')[0];
+let addExpencesValue = document.getElementsByClassName('additional_expenses-value')[0];
+let incomePeriodValue = document.getElementsByClassName('income_period-value')[0];
+let targetMonthValue = document.getElementsByClassName('target_month-value')[0];
 let salary = document.querySelector('.salary-amount');
 
 let incomeTitle = document.querySelector('.income-title');
