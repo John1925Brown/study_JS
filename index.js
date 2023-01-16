4300bc6eedca2a5452bb3605f7c3ddb2548e84b4
John1925Brown <dima.yaroshevskiy25@mail.ru>
2023-01-16 15:40:57 +0300

    Урок 10. Работа с объектами DOM​
    Практика

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ let ul = document.querySelectorAll('ul');
 let listSecElems = ul[0].querySelectorAll('li');
 let listFifElems = ul[5].querySelectorAll('li');
 let listSixElems = ul[2].querySelectorAll('li');
+let titles = document.querySelectorAll('h2');
 
-book[0].before(book[1]); // Книги по порядку
-book[0].after(book[4]);
-book[4].after(book[3]);
-book[3].after(book[5]);
-book[5].after(book[2]);
+
+const arrBook = Object.keys(book).sort((last, next) => {
+  if (book[last].firstElementChild.textContent > book[next].firstElementChild.textContent) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+
+for (let i = 0; i < arrBook.length; i++) {
+  books.append(book[arrBook[i]]);
+}
 
 let newTitle = document.createElement('a'); // Изменение заголовка
 newTitle.href = href = "https://github.com/azat-io/you-dont-know-js-ru/blob/master/up%20%26%20going/README.md#%D0%92%D1%8B-%D0%BD%D0%B5-%D0%B7%D0%BD%D0%B0%D0%B5%D1%82%D0%B5-js-%D0%9D%D0%B0%D1%87%D0%BD%D0%B8%D1%82%D0%B5-%D0%B8-%D0%A1%D0%BE%D0%B2%D0%B5%D1%80%D1%88%D0%B5%D0%BD%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5%D1%81%D1%8C";
@@ -23,7 +31,7 @@ adv.style.display = "none"; // Убрать рекламу
 
 document.body.style.backgroundImage = "url(./image/you-dont-know-js.jpg)"; // Поменять фон
 
-ul[0].prepend(listSecElems[10]);// Порядок глав в книгах
+ul[0].prepend(listSecElems[10]); // Порядок глав в книгах
 ul[0].prepend(listSecElems[2]);
 ul[0].prepend(listSecElems[9]);
 ul[0].prepend(listSecElems[7]);
