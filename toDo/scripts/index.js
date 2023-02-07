be47840cf79a2ccdd7ed859e40358a8ab9b1c37b
John1925Brown <dima.yaroshevskiy25@mail.ru>
2023-02-07 17:59:08 +0300

    Урок 12. Storage
    Практика

--- toDo/scripts/index.js
+++ toDo/scripts/index.js
@@ -1,18 +1,16 @@
-const todoControl = document.querySelector('.todo-control');
-const headerInput = document.querySelector('.header-input');
-const todoList = document.querySelector('.todo-list');
-const todoCompleted = document.querySelector('.todo-completed');
+let todoControl = document.querySelector('.todo-control');
+let headerInput = document.querySelector('.header-input');
+let todoList = document.querySelector('.todo-list');
+let todoCompleted = document.querySelector('.todo-completed');
+let btnAddTask = document.querySelector('.header-button');
+let todoData = [];
 
-
-const todoData = [];
-
-const render = function () {
+let render = function () {
   todoList.textContent = '';
   todoCompleted.textContent = '';
-
-  todoData.forEach(item => {
-    const li = document.createElement('li');
-    li.classList.add('todo-item');
+  todoData.forEach((item, index) => {
+    let li = document.createElement('li');
+    li.classList.add('todo-item', 'todo-item_' + index);
     li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
       '<div class="todo-buttons">' + '<button class="todo-remove"></button>' +
       '<button class="todo-complete"></button>' + '</div>';
@@ -21,24 +19,41 @@ const render = function () {
     } else {
       todoList.append(li);
     }
-    const BtnTodoComplete = li.querySelector('.todo-complete');
+    let BtnTodoComplete = li.querySelector('.todo-complete');
+    let BtnTodoRemove = li.querySelector('.todo-remove');
+    BtnTodoRemove.addEventListener('click', function () {
+      li.remove(item);
+      todoData.splice(index, 1);
+      localStorage.setItem('todo', JSON.stringify(todoData));
+    });
     BtnTodoComplete.addEventListener('click', function () {
       item.completed = !item.completed;
+      localStorage.setItem('todo', JSON.stringify(todoData));
       render();
     });
   });
 };
 
+if (localStorage.getItem('todo')) {
+  todoData = JSON.parse(localStorage.getItem('todo'));
+  render();
+};
 
 todoControl.addEventListener('submit', function (e) {
   e.preventDefault();
-  const newTodo = {
-    value: headerInput.value,
-    completed: false
-  };
-
-  todoData.push(newTodo);
-  render();
+  if (headerInput.value === '') {
+    alert('Заполните поле задачи');
+  } else {
+    let newTodo = {
+      value: headerInput.value,
+      completed: false
+    };
+    todoData.push(newTodo);
+    headerInput.value = '';
+    console.log(todoData);
+    localStorage.setItem('todo', JSON.stringify(todoData));
+    render();
+  }
 });
 
 render();
