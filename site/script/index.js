a36316fa1391be5108f271a25a445a49383b59da
John1925Brown <dima.yaroshevskiy25@mail.ru>
2023-06-15 02:40:09 +0300

    Урок 18. Скрипты и время выполнения
    Практика. Исправление

--- site/script/index.js
+++ site/script/index.js
@@ -5,11 +5,15 @@ window.addEventListener('DOMContentLoaded', function () {
       let timerHours = document.querySelector('#timer-hours');
       let timerMinutes = document.querySelector('#timer-minutes');
       let timerSeconds = document.querySelector('#timer-seconds');
+      let interval;
 
       function getTimeRemaining() {
       let dateStop = new Date(deadline).getTime();
       let dateNow = new Date().getTime();
       let timeRemaining = (dateStop - dateNow) / 1000;
+      if(timeRemaining < 0){
+        timeRemaining = 0;
+      }
       let seconds = Math.floor(timeRemaining % 60);
       let minutes = Math.floor((timeRemaining / 60) % 60);
       let hours = Math.floor(timeRemaining / 60 / 60);
@@ -19,19 +23,14 @@ window.addEventListener('DOMContentLoaded', function () {
 
       function updateClock() {
         let timer = getTimeRemaining();
-
-        if(timer.timeRemaining > 0){
-          setInterval(updateClock, 1000);
-        } else{
-          timer.hours = 0;
-          timer.minutes = 0;
-          timer.seconds = 0;
-        }
-
         timerHours.textContent = timer.hours;
         timerMinutes.textContent = timer.minutes;
         timerSeconds.textContent = timer.seconds;
-        
+
+        if(timer.timeRemaining < 0){
+          clearInterval(interval);
+        }
+
         if(timer.seconds < 10){
           timerSeconds.textContent = '0' + timer.seconds;
         }
@@ -42,15 +41,11 @@ window.addEventListener('DOMContentLoaded', function () {
           timerHours.textContent = '0' + timer.hours;
         }
       }
-      updateClock();
+
+      interval = setInterval(updateClock, 1000);
     }
 
-  countTimer('25 may 2023');
-
-
-
-
-
-
+  countTimer('10-june-2023');
+  // countTimer('20-june-2023');
 
 });
