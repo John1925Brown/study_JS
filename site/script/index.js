aed9b054ba753b8e645c934f0f8ba07ca223dd0d
John1925Brown <dima.yaroshevskiy25@mail.ru>
2023-06-21 17:21:26 +0300

    Урок 20. Делегирование
    Задание + усложненное

--- site/script/index.js
+++ site/script/index.js
@@ -95,7 +95,7 @@ const togglePopup = () => {
   const popUp = document.querySelector('.popup');
   const popUpBtns = document.querySelectorAll('.popup-btn');
 
-document.addEventListener('click', () => {
+document.addEventListener('click', (event) => {
     let target = event.target;
     if(popUp.style.display === 'block'){
       if(target.classList.contains('popup-close')){
